bfdcb9b6757933dded22de86de1aa9a852d67bc8
Tim Lucas <t.lucas@toolmantim.com>
2009-01-04 19:02:32 +1100

    New favicon mmkay

--- toolmantim.rb
+++ toolmantim.rb
@@ -37,6 +37,9 @@ helpers do
   def versioned_js(js)
     "/javascripts/#{js}.js?" + File.mtime(File.join(Sinatra.application.options.public, "javascripts", "#{js}.js")).to_i.to_s
   end
+  def versioned_favicon
+    "/favicon.ico?" + File.mtime(File.join(Sinatra.application.options.public, "favicon.ico")).to_i.to_s
+  end
   def partial(name)
     haml(:"_#{name}", :layout => false)
   end
