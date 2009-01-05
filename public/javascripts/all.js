3d0dd07680380c65ccaef227a387eb12915d6882
Tim Lucas <t.lucas@toolmantim.com>
2009-01-05 19:37:37 +1100

    Add the subject of the article to the mailto: link for article feedback

--- public/javascripts/all.js
+++ public/javascripts/all.js
@@ -6,8 +6,13 @@ $(function() {
 // Email links
 $(function() {
   $(".email").each(function() {
-    var newHtml = $(this).html().replace("#{thisdomain}", "toolmantim.com");
-    $(this).html(newHtml).wrap("<a href='mailto:" + newHtml + "'></a>");
+    var html = $(this).html().replace("#{thisdomain}", "toolmantim.com");
+    $(this).html(html).wrap("<a href='mailto:" + html + "'></a>");;
+  });
+  // Article feedback has subject toi
+  $(".article .feedback a .email").each(function() {
+    var a = $(this).parent();
+    a.attr("href", a.attr("href") + "?subject=Re: " + $("title").text());
   });
 });
 
